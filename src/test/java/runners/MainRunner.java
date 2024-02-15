dad2bda3f0843ab2eb574a3ec7762e84b670764d
Marcelo Dias <marcelodkarmo@gmail.com>
2024-02-15 20:16:15 -0300

    Camel case snnipet added to configs

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -2,9 +2,10 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberOptions.SnippetType;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        monochrome = true, dryRun = false,
+        monochrome = true, dryRun = false, snippets = SnippetType.CAMELCASE,
         plugin = {"pretty", "html:target/reports/cucumber.html", "json:target/reports/cucumber.json"},
         tags = "@login")
 public class MainRunner extends AbstractTestNGCucumberTests {
