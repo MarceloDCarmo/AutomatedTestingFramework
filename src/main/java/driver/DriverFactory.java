966f1ebf57409f86112026f3b66ec17149d7dc68
Marcelo Dias <marcelodkarmo@gmail.com>
2024-02-15 22:21:21 -0300

    DriverFactory

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,67 @@
+package driver;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DriverFactory {
+    private static final ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
+
+    public static WebDriver getDriver(){
+        if(webDriver.get() == null){
+            webDriver.set(createWebdriver());
+        }
+
+        return webDriver.get() ;
+    }
+
+    private static WebDriver createWebdriver() {
+        WebDriver driver = null;
+
+        String browserType = "chrome";
+
+        switch (browserType) {
+            case "chrome" -> {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/driver/drivers/chromedriver" + (System.getProperty("os.name").contains("win") ? ".exe" : ""));
+                ChromeOptions chromeOptions = getChromeOptions();
+                driver = new ChromeDriver(chromeOptions);
+            }
+            case "firefox" -> {
+                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/main/java/driver/drivers/geckodriver" + (System.getProperty("os.name").contains("win") ? ".exe" : ""));
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new FirefoxDriver(firefoxOptions);
+            }
+        }
+
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+
+    private static ChromeOptions getChromeOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.setBinary("/home/marcelo/Downloads/google-chrome/chrome");
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("disable-infobars"); // disabling info bars
+        chromeOptions.addArguments("--disable-extensions"); // disabling extensions
+        chromeOptions.addArguments("--disable-gpu"); // applicable to Windows os only
+        chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        chromeOptions.addArguments("--no-sandbox"); // Bypass OS security model
+        return chromeOptions;
+    }
+
+    public static void cleanupDriver(){
+        webDriver.get().quit();
+        webDriver.remove();
+    }
+}
