49d6db6d2a7a9a76096a21fcfc923b25d31aeddf
Marcelo Dias <marcelodkarmo@gmail.com>
2024-02-17 00:09:42 -0300

    Added parallel test runs configuration

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -3,10 +3,16 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.CucumberOptions.SnippetType;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
         monochrome = true, dryRun = false, snippets = SnippetType.CAMELCASE,
         plugin = {"pretty", "html:target/reports/cucumber.html", "json:target/reports/cucumber.json"},
-        tags = "@regression")
+        tags = "")
 public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
