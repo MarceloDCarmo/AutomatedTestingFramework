dd12cec2ce31c698e339c9a7ac17da392415bcc7
Marcelo Dias <marcelodkarmo@gmail.com>
2024-02-17 00:07:00 -0300

    Hook to attach screenshots to failed scenarios on reports

--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -2,8 +2,14 @@ package stepDefinitions.base;
 
 import driver.DriverFactory;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.sql.Timestamp;
 
 public class Hooks {
     protected WebDriver driver;
@@ -11,6 +17,19 @@ public class Hooks {
     @Before()
     public void setUp() {
         DriverFactory.getDriver();
+    }
+
+    @AfterStep()
+    public void captureExceptionImage(Scenario scenario){
+        if(scenario.isFailed()){
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String screenshotName = "Screenshot_" + Long.toString(timestamp.getTime());
+
+            byte[] screenshot = ((TakesScreenshot)DriverFactory.getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", screenshotName);
+        }
     }
 
     @After()
